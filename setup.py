cb5013e9ba82c2d5cd9d3e2f9568413569ffa4a7
Zafpyr <Zafpyr@hotmail.com>
2023-02-08 22:31:20 +1300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bg-dloadr',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/Zafpyr/bg-dloadr',
+    license='GPL V3',
+    author='Zafpyr',
+    author_email='',
+    description='Bulk download wallpapers'
+)
